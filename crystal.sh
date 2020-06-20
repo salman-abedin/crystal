85f43ae88dda794368d8db2c4283ad4b5c3be989
Salman Abedin <salmanabedin@zohomail.com>
2020-06-20 11:54:54 +0600

    update

--- crystal.sh
+++ crystal.sh
@@ -16,56 +16,30 @@ bspc config gapless_monocle true
 #                             Script
 ###############################################################################
 
-hidden_windows=/tmp/crystal_0$(wmctrl -d | grep "\*" | cut -d ' ' -f 1)
-mode="$hidden_windows"_mode
-
-hide_nodes() {
-    xdo id -rd | tee "$hidden_windows"
-    [ -s "$hidden_windows" ] && cat "$hidden_windows" | xargs xdo hide
-}
-
-unhide_nodes() {
-    cat "$hidden_windows" | xargs xdo show
-    : > "$hidden_windows"
-}
-
+mode=/tmp/crystal_0$(wmctrl -d | grep "\*" | cut -d ' ' -f 1)
 
 case $1 in
 
     --navigate)
-
-        # Regular navigations (Put your window managers navigations here)
         if [ "$2" = "next" ]; then
-            bspc node -f next.local && exit 0
+            window=$(xdo id -rd | head -1)
         else
-            bspc node -f prev.local && exit 0
-            # xdo id -rd | head -1 | xargs xdo activate && exit 0
+            # todo: previous window
+            window=$(xdo id -rd | head -1)
         fi
 
-        # Edge cases
-        [ ! -f "$hidden_windows" ] && exit 1
-        [ "$(wc -l < "$hidden_windows")" = 0 ] && exit 1
-
-        # This is where the fun begins
-
-        current_window=$(xdo id)
-        xdo hide
-
-        if [ "$2" = "next" ]; then
-            tail -1 "$hidden_windows" | xargs xdo show
-            sed -i '$d' "$hidden_windows"
-            printf "%s\n%s" "$current_window" "$(cat "$hidden_windows")" > "$hidden_windows"
+        if [ -s "$mode" ] && xdo id -rd; then
+            xdo hide
+            xdo show "$window"
         else
-            head -1 "$hidden_windows" | xargs xdo show
-            sed -i '1d' "$hidden_windows"
-            echo "$current_window" >> "$hidden_windows"
+            xdo activate "$window"
         fi
         ;;
 
     --toggle)
         if [ -s "$mode" ]; then
             : > "$mode"
-            unhide_nodes
+            xdo id -rd && xdo id -rd | xargs xdo show
             bspc desktop -l tiled
             if [ "$2" = "fullscreen" ]; then
                 xdo show -a $STATUS_BAR
@@ -73,7 +47,7 @@ case $1 in
             fi
         else
             echo mono > "$mode"
-            hide_nodes
+            xdo id -rd && xdo id -rd | xargs xdo hide
             bspc desktop -l monocle
             if [ "$2" = "fullscreen" ]; then
                 xdo hide -a $STATUS_BAR
@@ -86,10 +60,7 @@ case $1 in
 
     --close)
         xdo close
-        if [ -s "$hidden_windows" ]; then
-            tail -1 "$hidden_windows" | xargs xdo show
-            sed -i '$d' "$hidden_windows"
-        fi
+        [ -s "$mode" ] && xdo id -rd && xdo id -rd | head -1 | xargs xdo show
         ;;
     *) : ;;
 
