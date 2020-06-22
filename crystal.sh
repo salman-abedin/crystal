6fee3a939bc0ac08489fb18a8c15ca48a22a8495
Salman Abedin <salmanabedin@zohomail.com>
2020-06-22 14:00:09 +0600

    Complete overhaul & added Makefile

--- crystal.sh
+++ crystal.sh
@@ -18,72 +18,81 @@ bspc config gapless_monocle true
 
 WORKSPACE=/tmp/crystal_0$(wmctrl -d | grep "\*" | cut -d ' ' -f 1)
 
-while :; do
+solo() {
     case $1 in
-        --navigate)
-            shift
-            # window=$(xdo id -rd | head -1)
-            if [ "$1" = "next" ]; then
+        on)
+            echo solo >> "$WORKSPACE"
+            xdo id -rd && xdo id -rd | xargs xdo hide
+            bspc desktop -l monocle
+            ;;
+        off)
+            sed -i 's/solo//' "$WORKSPACE"
+            xdo id -rd && xdo id -rd | xargs xdo show
+            bspc desktop -l tiled
+            ;;
+    esac
+    # $0 --navigate prev
+    xdo id -rd | head -1 | xargs xdo activate
+    # bspc node -f prev.local
+    bspc node -n biggest.local
+}
+
+case $1 in
+    --navigate)
+        shift
+        # window=$(xdo id -rd | head -1)
+        case $1 in
+            next)
                 window=$(xdo id -rd | tail -1)
                 # ! grep next "$WORKSPACE" && echo next >> "$WORKSPACE" && window=$(xdo id -rd | tail -1)
-
-            else
+                ;;
+            prev)
                 window=$(xdo id -rd | head -1)
                 # grep next "$WORKSPACE" && sed -i 's/next//' "$WORKSPACE" && window=$(xdo id -rd | tail -1)
-
-            fi
-            if grep solo "$WORKSPACE" && xdo id -rd; then
-                xdo hide
-                xdo show "$window"
-            else
-                xdo activate "$window"
-            fi
-            ;;
-        --toggle)
-            shift
-            case $1 in
-                solo)
-                    shift
-                    if grep solo "$WORKSPACE" || [ "$1" = off ]; then
-                        sed -i 's/solo//' "$WORKSPACE"
-                        xdo id -rd && xdo id -rd | xargs xdo show
-                        bspc desktop -l tiled
-                    else
-                        echo solo >> "$WORKSPACE"
-                        xdo id -rd && xdo id -rd | xargs xdo hide
-                        bspc desktop -l monocle
-                    fi
-                    ;;
-                fullscreen)
-                    $0 --toggle solo
-                    if grep fullscreen "$WORKSPACE"; then
-                        sed -i 's/fullscreen//' "$WORKSPACE"
-                        xdo show -a $STATUSBAR
-                        bspc config top_padding $top_padding
-                    else
-                        echo fullscreen >> "$WORKSPACE"
-                        xdo hide -a $STATUSBAR
-                        bspc config top_padding 0
-                    fi
-                    ;;
-            esac
-            # $0 --navigate prev
-            xdo id -rd | head -1 | xargs xdo activate
-            # bspc node -f prev.local
-            bspc node -n biggest.local
-            ;;
-        --close)
-            xdo close
-            if grep solo "$WORKSPACE"; then
-                if xdo id -rd; then
-                    xdo id -rd | head -1 | xargs xdo show
+                ;;
+        esac
+        if grep solo "$WORKSPACE" && xdo id -rd; then
+            xdo hide
+            xdo show "$window"
+        else
+            xdo activate "$window"
+        fi
+        ;;
+    --toggle)
+        shift
+        case $1 in
+            solo)
+                if grep solo "$WORKSPACE"; then
+                    solo off
                 else
-                    xdo show -a $STATUSBAR
-                    bspc config top_padding $top_padding
+                    solo on
                 fi
+                ;;
+            fullscreen)
+                if grep fullscreen "$WORKSPACE"; then
+                    sed -i 's/fullscreen//' "$WORKSPACE"
+                    xdo show -a $STATUSBAR
+                    solo off
+                    bspc config top_padding $top_padding
+                else
+                    echo fullscreen >> "$WORKSPACE"
+                    xdo hide -a $STATUSBAR
+                    solo on
+                    bspc config top_padding 0
+                fi
+                ;;
+        esac
+        ;;
+    --close)
+        xdo close
+        if grep solo "$WORKSPACE"; then
+            if xdo id -rd; then
+                xdo id -rd | head -1 | xargs xdo show
+            else
+                xdo show -a $STATUSBAR
+                bspc config top_padding $top_padding
             fi
-            ;;
-        *) : ;;
-    esac
-    shift
-done
+        fi
+        ;;
+    *) : ;;
+esac
